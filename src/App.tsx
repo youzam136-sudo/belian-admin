c5c88c1d71459bc3e666594c01c3bbbe9fe1798c
youzam136-sudo <youzam136@gmail.com>
2026-08-27 13:44:07 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,18 @@ import MembersPage from "./pages/MembersPage";
 import StaffPage from "./pages/StaffPage";
 import MemberSettingsPage from "./pages/MemberSettingsPage";
 import ProductsPage from "./pages/ProductsPage";
-import InventoryPage from "./pages/InventoryPage";
 import OrdersPage from "./pages/OrdersPage";
-import PaymentsPage from "./pages/PaymentsPage";
 import ShippingPage from "./pages/ShippingPage";
-import AdminsPage from "./pages/AdminsPage";
+import ProductInquiriesPage from "./pages/ProductInquiriesPage";
+import ReviewsPage from "./pages/ReviewsPage";
+import CouponsPage from "./pages/CouponsPage";
+import PointsPage from "./pages/PointsPage";
+import BoardPage from "./pages/BoardPage";
+import GeneralSettingsPage from "./pages/GeneralSettingsPage";
+import SeoSettingsPage from "./pages/SeoSettingsPage";
+import TermsSettingsPage from "./pages/TermsSettingsPage";
+import PaymentSettingsPage from "./pages/PaymentSettingsPage";
+import SecuritySettingsPage from "./pages/SecuritySettingsPage";
 
 function App() {
     return (
@@ -28,13 +35,11 @@ function App() {
                         element={<MembersPage />}
                         handle={{ title: "회원 목록" }}
                     />
-
                     <Route
                         path="members/staff"
                         element={<StaffPage />}
                         handle={{ title: "운영진" }}
                     />
-
                     <Route
                         path="members/settings"
                         element={<MemberSettingsPage />}
@@ -44,37 +49,70 @@ function App() {
                     <Route
                         path="products"
                         element={<ProductsPage />}
-                        handle={{ title: "상품 관리" }}
+                        handle={{ title: "상품" }}
                     />
-
-                    <Route
-                        path="inventory"
-                        element={<InventoryPage />}
-                        handle={{ title: "재고 관리" }}
-                    />
-
                     <Route
                         path="orders"
                         element={<OrdersPage />}
-                        handle={{ title: "주문 관리" }}
+                        handle={{ title: "주문" }}
                     />
-
-                    <Route
-                        path="payments"
-                        element={<PaymentsPage />}
-                        handle={{ title: "결제 관리" }}
-                    />
-
                     <Route
                         path="shipping"
                         element={<ShippingPage />}
-                        handle={{ title: "배송 관리" }}
+                        handle={{ title: "배송" }}
+                    />
+                    <Route
+                        path="products/inquiries"
+                        element={<ProductInquiriesPage />}
+                        handle={{ title: "상품문의" }}
+                    />
+                    <Route
+                        path="products/reviews"
+                        element={<ReviewsPage />}
+                        handle={{ title: "구매평" }}
                     />
 
                     <Route
-                        path="admins"
-                        element={<AdminsPage />}
-                        handle={{ title: "관리자 관리" }}
+                        path="promotions/coupons"
+                        element={<CouponsPage />}
+                        handle={{ title: "쿠폰" }}
+                    />
+                    <Route
+                        path="promotions/points"
+                        element={<PointsPage />}
+                        handle={{ title: "적립금" }}
+                    />
+
+                    <Route
+                        path="boards"
+                        element={<BoardPage />}
+                        handle={{ title: "게시판" }}
+                    />
+
+                    <Route
+                        path="settings/general"
+                        element={<GeneralSettingsPage />}
+                        handle={{ title: "일반" }}
+                    />
+                    <Route
+                        path="settings/seo"
+                        element={<SeoSettingsPage />}
+                        handle={{ title: "SEO" }}
+                    />
+                    <Route
+                        path="settings/terms"
+                        element={<TermsSettingsPage />}
+                        handle={{ title: "약관" }}
+                    />
+                    <Route
+                        path="settings/payment"
+                        element={<PaymentSettingsPage />}
+                        handle={{ title: "전자결제" }}
+                    />
+                    <Route
+                        path="settings/security"
+                        element={<SecuritySettingsPage />}
+                        handle={{ title: "보안 개인정보보호" }}
                     />
                 </Route>
             </Routes>
