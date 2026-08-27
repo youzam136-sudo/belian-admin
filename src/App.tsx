4483766b42c1188f9dfff9794ee28d5b96ec2931
youzam136-sudo <youzam136@gmail.com>
2026-08-27 12:36:08 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Layout from "./components/Layout";
 import DashboardPage from "./pages/DashboardPage";
 import MembersPage from "./pages/MembersPage";
 import StaffPage from "./pages/StaffPage";
-import MemberBehaviorPage from "./pages/MemberBehaviorPage";
 import MemberSettingsPage from "./pages/MemberSettingsPage";
-import MemberSupportPage from "./pages/MemberSupportPage";
 import ProductsPage from "./pages/ProductsPage";
 import InventoryPage from "./pages/InventoryPage";
 import OrdersPage from "./pages/OrdersPage";
@@ -38,21 +36,9 @@ function App() {
                     />
 
                     <Route
-                        path="members/behavior"
-                        element={<MemberBehaviorPage />}
-                        handle={{ title: "고객 행동 관리" }}
-                    />
-
-                    <Route
                         path="members/settings"
                         element={<MemberSettingsPage />}
                         handle={{ title: "고객 설정" }}
-                    />
-
-                    <Route
-                        path="members/support"
-                        element={<MemberSupportPage />}
-                        handle={{ title: "고객 상담 채널" }}
                     />
 
                     <Route
