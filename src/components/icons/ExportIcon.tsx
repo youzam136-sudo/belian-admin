cd248c40ee0cf5af0a2909186e7648ec4e915332
youzam136-sudo <youzam136@gmail.com>
2026-08-31 20:44:16 +0900

    Create ExportIcon.tsx

---
+++ src/components/icons/ExportIcon.tsx
@@ -0,0 +1,28 @@
+import type { SVGProps } from "react";
+
+/** 내보내기(다운로드) 아이콘 */
+export function ExportIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16.391 15.497"
+      {...props}
+    >
+      <path
+        d="M.962,11.412H-.75V0H.962Z"
+        transform="translate(8.089 0)"
+        fill="currentColor"
+      />
+      <path
+        d="M2514.177,488.126l-4.26-4.344,1.222-1.2,3.016,3.076,2.9-3.065,1.243,1.177Z"
+        transform="translate(-2505.909 -475.48)"
+        fill="currentColor"
+      />
+      <path
+        d="M2520.825,492.078h-11.863a1.934,1.934,0,0,1-1.449-.605,3.139,3.139,0,0,1-.674-2.141v-2.479h1.712l0,2.555a1.509,1.509,0,0,0,.213.894c.023.024.061.064.2.064h11.885c.309-.008.668-.194.668-1.013v-2.5h1.712v2.5a2.751,2.751,0,0,1-.775,2.077A2.424,2.424,0,0,1,2520.825,492.078Z"
+        transform="translate(-2506.837 -476.581)"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
