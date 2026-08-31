a90e0cf29480fa212d06dd341b8ade2383a49097
youzam136-sudo <youzam136@gmail.com>
2026-08-31 20:38:43 +0900

    Create LocationIcon.tsx

---
+++ src/components/icons/LocationIcon.tsx
@@ -0,0 +1,33 @@
+import type { SVGProps } from "react";
+
+/** 출고 및 반품/교환지 아이콘 */
+export function LocationIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 15.301 12.217"
+      {...props}
+    >
+      <path
+        d="M5.785,15.261a5.77,5.77,0,0,1-2.618-.631H.631V12.094a5.76,5.76,0,1,1,5.155,3.167m-4.35-1.435h1.93l.088.047a4.965,4.965,0,1,0-2.065-2.065l.047.088Z"
+        transform="translate(0 -3.044)"
+        fill="currentColor"
+      />
+      <path
+        d="M43.457,10.2h-.116l.018-.8a4.313,4.313,0,0,0,2.109-.5l.088-.047h1.651V7.2l.047-.088a4.3,4.3,0,0,0-6.19-5.576L40.615.864a5.1,5.1,0,0,1,7.4,6.529V9.651H45.754a5.111,5.111,0,0,1-2.3.545"
+        transform="translate(-33.263 0)"
+        fill="currentColor"
+      />
+      <path
+        d="M23.121,32.843a.6.6,0,1,1-.6-.6.6.6,0,0,1,.6.6"
+        transform="translate(-17.947 -26.404)"
+        fill="currentColor"
+      />
+      <path
+        d="M36.455,32.843a.6.6,0,1,1-.6-.6.6.6,0,0,1,.6.6"
+        transform="translate(-28.867 -26.404)"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
